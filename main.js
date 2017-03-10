b881c9d12129cef996cd84d3a6bd6f6a28b0a7d9
flieh <ruedelaflieh@gmail.com>
2017-03-10 14:44:09 +0100

    htaccess

--- main.js
+++ main.js
@@ -1,49 +1,34 @@
 'use strict'
-$(document).ready(function() {
-  $.get('pages/logo.html', function(data) {
+$(document).ready(function () {
+  $.get('pages/logo.html', function (data) {
     $('header').html(data)
   })
-  $.get('pages/nav.html', function(data) {
+  $.get('pages/nav.html', function (data) {
     $('nav').html(data)
     $('#accueil').children('button').removeClass('btn-primary').addClass('btn-default')
-    $('#accueil').children('button').click(function() {
-      if (!$('#accueil').children('button').hasClass('btn-default')) {
-        $('button').removeClass('btn-default').addClass('btn-primary')
-        $('#accueil').children('button').removeClass('btn-primary').addClass('btn-default')
-        $.get('pages/accueil.html', function(data) {
-          $('main').html(data)
-        })
-      }
+    $('#accueil').children('button').on('click', function () {
+      $('button').removeClass('btn-default').addClass('btn-primary')
+      $('#accueil').children('button').removeClass('btn-primary').addClass('btn-default')
+      $.get('pages/accueil.html', function (data) {
+        $('main').html(data)
+      })
     })
-    $('#la-carte').children('button').click(function() {
-      if (!$('#la-carte').children('button').hasClass('btn-default')) {
-        $('button').removeClass('btn-default').addClass('btn-primary')
-        $('#la-carte').children('button').removeClass('btn-primary').addClass('btn-default')
-        $.get('pages/carte.html', function(data) {
-          $('main').html(data)
-        })
-      }
+    $('#la-carte').children('button').on('click', function () {
+      $('button').removeClass('btn-default').addClass('btn-primary')
+      $('#la-carte').children('button').removeClass('btn-primary').addClass('btn-default')
+      $.get('pages/carte.html', function (data) {
+        $('main').html(data)
+      })
     })
-    $('#plan-acces').children('button').click(function() {
-      if (!$('#plan-acces').children('button').hasClass('btn-default')) {
-        $('button').removeClass('btn-default').addClass('btn-primary')
-        $('#plan-acces').children('button').removeClass('btn-primary').addClass('btn-default')
-        $.get('pages/plan.html', function(data) {
-          $('main').html(data)
-        })
-      }
-    })
-    $('#avis').children('button').click(function() {
-      if (!$('#avis').children('button').hasClass('btn-default')) {
-        $('button').removeClass('btn-default').addClass('btn-primary')
-        $('#avis').children('button').removeClass('btn-primary').addClass('btn-default')
-        $.get('pages/avis.html', function(data) {
-          $('main').html(data)
-        })
-      }
+    $('#plan-acces').children('button').on('click', function () {
+      $('button').removeClass('btn-default').addClass('btn-primary')
+      $('#plan-acces').children('button').removeClass('btn-primary').addClass('btn-default')
+      $.get('pages/plan.html', function (data) {
+        $('main').html(data)
+      })
     })
   })
-  $.get('pages/diapo.html', function(data) {
+  $.get('pages/accueil.html', function (data) {
     $('main').html(data)
   })
 })
