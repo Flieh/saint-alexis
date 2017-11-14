ec27c4735d668b912ec043507d79150a427d16fb
flieh <ruedelaflieh@gmail.com>
2017-11-14 08:34:07 +0100

    fix ajax calls

--- main.js
+++ main.js
@@ -1,51 +1,49 @@
 'use strict'
 $(window).ready(function () {
-  $.get('pages/logo.html', function (data) {
-    $('header').html(data)
-  })
-  $.get('pages/nav.html', function (data) {
-    $('nav').html(data)
-    $('#accueil').children('button').removeClass('btn-primary').addClass('btn-default')
-    $('#accueil').children('button').on('click', function () {
-      $('button').removeClass('btn-default').addClass('btn-primary')
-      $('#accueil').children('button').removeClass('btn-primary').addClass('btn-default')
-      $.get('pages/accueil.html', function (data) {
-        $('main').html(data)
-        $(document).ready(function () {
-          $('.slider').bxSlider({
-            auto: true,
-            pager: false
+  $.get('pages/logo.html', function (logo) {
+    $.get('pages/nav.html', function (nav) {
+      $.get('pages/accueil.html', function (accueil) {
+        $.get('pages/carte.html', function (carte) {
+          $.get('pages/plan.html', function (plan) {
+            $('header').html(logo)
+            $('nav').html(nav)
+            $('#accueil').children('button').removeClass('btn-primary').addClass('btn-default')
+            $('#accueil').children('button').on('click', function () {
+              $('button').removeClass('btn-default').addClass('btn-primary')
+              $('#accueil').children('button').removeClass('btn-primary').addClass('btn-default')
+              $('main').html(accueil)
+              $(document).ready(function () {
+                $('.slider').bxSlider({
+                  auto: true,
+                  pager: false
+                })
+              })
+            })
+            $('#la-carte').children('button').on('click', function () {
+              $('button').removeClass('btn-default').addClass('btn-primary')
+              $('#la-carte').children('button').removeClass('btn-primary').addClass('btn-default')
+              $('main').html(carte)
+              $(document).ready(function () {
+                $('.slider').bxSlider({
+                  auto: true,
+                  pager: false
+                })
+              })
+            })
+            $('#plan-acces').children('button').on('click', function () {
+              $('button').removeClass('btn-default').addClass('btn-primary')
+              $('#plan-acces').children('button').removeClass('btn-primary').addClass('btn-default')
+              $('main').html(plan)
+            })
+            $('main').html(accueil)
+            $(document).ready(function () {
+              $('.slider').bxSlider({
+                auto: true,
+                pager: false
+              })
+            })
           })
         })
-      })
-    })
-    $('#la-carte').children('button').on('click', function () {
-      $('button').removeClass('btn-default').addClass('btn-primary')
-      $('#la-carte').children('button').removeClass('btn-primary').addClass('btn-default')
-      $.get('pages/carte.html', function (data) {
-        $('main').html(data)
-        $(document).ready(function () {
-          $('.slider').bxSlider({
-            auto: true,
-            pager: false
-          })
-        })
-      })
-    })
-    $('#plan-acces').children('button').on('click', function () {
-      $('button').removeClass('btn-default').addClass('btn-primary')
-      $('#plan-acces').children('button').removeClass('btn-primary').addClass('btn-default')
-      $.get('pages/plan.html', function (data) {
-        $('main').html(data)
-      })
-    })
-  })
-  $.get('pages/accueil.html', function (data) {
-    $('main').html(data)
-    $(document).ready(function () {
-      $('.slider').bxSlider({
-        auto: true,
-        pager: false
       })
     })
   })
